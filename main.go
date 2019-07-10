3a5578bb6cd6feac4932f93e58241458831504f5
Joao Henrique Machado Silva <joaoh82@gmail.com>
2019-07-10 19:33:16 +0200

    Permutation of a Palindrome

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("String: hello", IsStringCharsUnique("hello"))
 	fmt.Println("String: dog", IsStringCharsUnique("dog"))
+
+	fmt.Println("String: qqwweerrt", IsPermutationOfPalindrome("qqwweerrt"))
+	fmt.Println("String: qqwweerrta", IsPermutationOfPalindrome("qqwweerrta"))
 }
 
 func IsStringCharsUnique(input string) bool {
@@ -16,6 +19,30 @@ func IsStringCharsUnique(input string) bool {
 			return false
 		} else {
 			charMap[string(input[i])] = true
+		}
+	}
+
+	return true
+}
+
+func IsPermutationOfPalindrome(input string) bool {
+	charMap := map[string]int{}
+
+	for _, v := range input {
+		if _, exists := charMap[string(v)]; exists {
+			charMap[string(v)]++
+		} else {
+			charMap[string(v)] = 1
+		}
+	}
+
+	foundOdd := false
+	for _, value := range charMap {
+		if value%2 == 1 {
+			if foundOdd {
+				return false
+			}
+			foundOdd = true
 		}
 	}
 
